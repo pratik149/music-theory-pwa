b5bf691666aa8f0051abccdfd107641d68b03f69
Pratik Rane <pratikrane149@gmail.com>
2019-04-24 09:33:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
+const cacheName = 'cache-v1';
+const resourcesToPrechace = [
+	'/',
+	'index.html',
+	'styles/style.css',
+	'scripts/script.js',
+	'scripts/bootstrap.min.css',
+	'scripts/bootstrap.min.js',
+	'scripts/jquery-3.4.0.js',
+	'assets/music-player.svg'.
+];
+
+self.addEventListener('install', event => {
+	console.log('Service Worker Install event');
+	event.waitUntil(
+		caches.open(cacheName)
+		.then(cache => {
+			return cache.addAll(resourcesToPrechace);
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	console.log('Activate event');
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(caches.match(event.request)
+		.then(cachedResponse => {
+			return cachedResponse || fetch(event.request);
+		})
+	);
+	console.log('Fetch intercepted for:', event.request.url);
+});
+
 
